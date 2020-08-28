542cbceda15a63308fdd8ef9e3562ee5be2d4b5d
Mohammad Shishegar <mohammad127sh82@gmail.com>
2020-08-28 18:59:55 +0430

    Updates

---
+++ precache-manifest.e23fa59e1427389d3c7ae88479c1351e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe1009d3d094b79aa84bb80364e36b3",
+    "url": "/Trello/index.html"
+  },
+  {
+    "revision": "f0b1d90a0678feb6760a",
+    "url": "/Trello/static/css/main.5a1b95e7.chunk.css"
+  },
+  {
+    "revision": "936804ff2219f901c8cb",
+    "url": "/Trello/static/js/2.4162f687.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trello/static/js/2.4162f687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b1d90a0678feb6760a",
+    "url": "/Trello/static/js/main.1cd459d4.chunk.js"
+  },
+  {
+    "revision": "0366f2285cd209cec469",
+    "url": "/Trello/static/js/runtime-main.d549d69f.js"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/Trello/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/Trello/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/Trello/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "a2a26d7906c257d584688e4a0130fe8c",
+    "url": "/Trello/static/media/user.a2a26d79.jpg"
+  }
+]);
